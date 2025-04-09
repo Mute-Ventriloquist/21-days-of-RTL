9c89cedc1a769e08b81559976ab6ead0c91fce2f
Venkateswaran <emailvenkat006@gmail.com>
2025-04-09 09:31:10 +0530

    added day 1

---
+++ day1/mux.v
@@ -0,0 +1,17 @@
+// A simple mux
+
+`timescale 1ns/1ps
+
+module mux (
+  input   wire [7:0]    a_i,
+  input   wire [7:0]    b_i,
+  input   wire          sel_i,
+  output  wire [7:0]    y_o
+);
+
+  // Write your logic here...
+  assign y_o = sel_i ? b_i : a_i;
+
+  
+endmodule
+
